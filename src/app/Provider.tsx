227fdf69313b69b82597255cca83bdeb27065356
rious275 <rious275@gmail.com>
2023-10-13 02:11:54 +0900

    fix: react-query 포커스 요청 off

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -4,7 +4,13 @@ import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
 
   return (
     <RecoilRoot>
